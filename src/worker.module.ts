d595b211e433c0aba837154d95cedb20b20d8574
Alejandro <alejandrovillacordero@gmail.com>
2025-12-20 07:26:54 -0400

    Renamed workers folder

--- src/worker.module.ts
+++ src/worker.module.ts
@@ -5,13 +5,13 @@ import { HubspotModule } from './modules/integrations/hubspot/hubspot.module';
 import { RagModule } from './modules/rag/rag.module';
 import { GoogleModule } from './modules/integrations/google/google.module';
 
-import { HubspotContactsSyncWorker } from './worker/hubspot-contacts-sync.worker';
-import { RagEmbedWorker } from './worker/rag-embed.worker';
-import { HubspotNotesSyncWorker } from './worker/hubspot-notes-sync.worker';
-import { SyncSchedulerService } from './worker/sync-scheduler.service';
-import { SyncTickWorker } from './worker/sync-tick.worker';
-import { GmailSyncWorker } from './worker/gmail-sync.worker';
-import { CalendarSyncWorker } from './worker/calendar-sync.worker';
+import { HubspotContactsSyncWorker } from './workers/hubspot-contacts-sync.worker';
+import { RagEmbedWorker } from './workers/rag-embed.worker';
+import { HubspotNotesSyncWorker } from './workers/hubspot-notes-sync.worker';
+import { SyncSchedulerService } from './workers/sync-scheduler.service';
+import { SyncTickWorker } from './workers/sync-tick.worker';
+import { GmailSyncWorker } from './workers/gmail-sync.worker';
+import { CalendarSyncWorker } from './workers/calendar-sync.worker';
 
 @Module({
   imports: [DbModule, JobsModule, HubspotModule, RagModule, GoogleModule],
